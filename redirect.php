86596684d4eb38f62def800eb76e06b16e6af816
Your Name <henriques.gabriell@gmail.com>
2019-02-27 08:18:16 -0300

    added htaccess and minor modification

--- redirect.php
+++ redirect.php
@@ -4,9 +4,12 @@ include "connection.php";
 $link = explode('/',$_SERVER['REQUEST_URI']);
 
 try{
-		$stmt = $conn->prepare('SELECT url FROM url WHERE short = :short');
-		$stmt->bindParam(':short',$link[0]);
-		$stmt->execute();
-	}catch(PDOException $e){
-		echo $sql . "<br>" . $e->getMessage();
-	}
+	$stmt = $conn->prepare('SELECT url FROM url WHERE short = :short');
+	$stmt->bindParam(':short',$link[0]);
+	$stmt->execute();
+	$result = $stmt->fetch();
+}catch(PDOException $e){
+	echo $sql . "<br>" . $e->getMessage();
+}
+
+header('Location: '.$result['1']['url']);
